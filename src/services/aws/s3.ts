dcf9c7a703bb25c32939effba65fd74df8e4d355
Akhil Bisht <akhilbisht83@gmail.com>
2023-01-04 13:21:30 +0530

    add: list s3 media

--- src/services/aws/s3.ts
+++ src/services/aws/s3.ts
@@ -1,5 +1,6 @@
 import S3 from "aws-sdk/clients/s3";
 import { v4 as uuid } from "uuid";
+import ErrorHandler from "../../utils/errorHandler";
 
 const bucket = new S3();
 const BUCKET_NAME = process.env.BUCKET_NAME;
@@ -13,4 +14,18 @@ export const putMedia = async (file: any, location: string, bucketName?: string)
     ContentType: file.mimeType,
   };
   return await bucket.upload(params).promise();
+};
+
+export const listMedia = async (Bucket: string, Location: string) => {
+  bucket.listObjectVersions(
+    {
+      Bucket,
+      Prefix: Location,
+    },
+    (err, data) => {
+      if (err) throw new ErrorHandler(err.message, err.statusCode ? err.statusCode : 500);
+      console.log(data);
+      return data;
+    }
+  );
 };
