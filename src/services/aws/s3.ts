1d39e9a78339667e6b1adee01dce46de7b4f916b
Akhil Bisht <akhilbisht83@gmail.com>
2023-01-04 16:28:42 +0530

    update: add mime extension

--- src/services/aws/s3.ts
+++ src/services/aws/s3.ts
@@ -1,4 +1,4 @@
-import S3 from "aws-sdk/clients/s3";
+import S3, { PutObjectRequest } from "aws-sdk/clients/s3";
 import { v4 as uuid } from "uuid";
 import ErrorHandler from "../../utils/errorHandler";
 
@@ -7,25 +7,15 @@ const BUCKET_NAME = process.env.BUCKET_NAME;
 
 export const putMedia = async (file: any, location: string, bucketName?: string) => {
   const fileName = uuid() + "-" + Date.now();
-  const params = {
+  const params: PutObjectRequest = {
     Bucket: `${bucketName ? bucketName : BUCKET_NAME}/${location}`,
     Body: file.buffer,
-    Key: fileName,
-    ContentType: file.mimeType,
+    Key: `${fileName}.${file.mimetype.split("/")[1]}`,
+    ContentType: file.mimetype,
   };
   return await bucket.upload(params).promise();
 };
 
 export const listMedia = async (Bucket: string, Location: string) => {
-  bucket.listObjectVersions(
-    {
-      Bucket,
-      Prefix: Location,
-    },
-    (err, data) => {
-      if (err) throw new ErrorHandler(err.message, err.statusCode ? err.statusCode : 500);
-      console.log(data);
-      return data;
-    }
-  );
+  return await bucket.listObjectsV2({ Bucket, Prefix: Location }).promise();
 };
