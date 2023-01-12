5fcb12759d9bc36391e871fb0f1da502f63ed5cf
Akhil Bisht <akhilbisht83@gmail.com>
2023-01-12 13:12:51 +0530

    edit : add estimate pdf creator to seed

--- src/seed/seed.ts
+++ src/seed/seed.ts
@@ -1,6 +1,9 @@
+import { ObjectId } from "mongodb";
 import { createSearchIndex, createUniqueServiceIndex } from "../modules/service/crud";
+import generateEstimate from "../modules/ticket/estimate/createEstimate";
 
 export default async function () {
-  await createSearchIndex();
-  await createUniqueServiceIndex();
+  false && (await createSearchIndex());
+  false && (await createUniqueServiceIndex());
+  false && (await generateEstimate(new ObjectId("63bfaf8f7b190547681994fa")));
 }
