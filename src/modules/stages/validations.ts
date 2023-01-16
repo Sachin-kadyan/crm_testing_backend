4bf52bf35801d57b96e2aed7c6d7b277066b8660
Akhil Bisht <akhilbisht83@gmail.com>
2023-01-16 13:02:01 +0530

    edit: stage parent null | id

--- src/modules/stages/validations.ts
+++ src/modules/stages/validations.ts
@@ -1,8 +1,13 @@
 import { body } from "express-validator";
+import { ObjectId } from "mongodb";
 
 export const create = [
   body("name").notEmpty().isString(),
   body("description").notEmpty().isString(),
-  body("parent").optional().notEmpty().isString(),
+  body("parent")
+    .optional({ nullable: true })
+    .isHexadecimal()
+    .bail()
+    .customSanitizer((value) => new ObjectId(value)),
   body("code").notEmpty().isNumeric(),
 ];
