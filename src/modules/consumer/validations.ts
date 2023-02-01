96cd260d67c9c773bd3fabdd38758bb7d22dad6b
Akhil Bisht <akhilbisht83@gmail.com>
2023-02-01 17:00:56 +0530

    edit: email, age, lastname patient register

--- src/modules/consumer/validations.ts
+++ src/modules/consumer/validations.ts
@@ -2,15 +2,16 @@ import { body } from "express-validator";
 
 export const create = [
   body("firstName").notEmpty().toLowerCase(),
-  body("lastName").notEmpty().toLowerCase(),
-  body("email").isEmail().toLowerCase(),
-  body("dob").toDate().notEmpty(),
+  body("lastName").optional({ nullable: true }).notEmpty().toLowerCase(),
+  body("email").optional({ nullable: true }).isEmail().toLowerCase(),
+  body("dob").optional({ nullable: true }).toDate().notEmpty(),
   body("phone")
     .isLength({ max: 10, min: 10 })
     .isMobilePhone("en-IN")
     .customSanitizer((value) => "91" + value),
   body("uid").notEmpty(),
   body("gender")
+    .optional({ nullable: true })
     .notEmpty()
     .custom((value, { req }) => {
       if (value !== "M" && value !== "F" && value !== "O") {
