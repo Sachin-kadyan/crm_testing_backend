4842583eb87a6b6dc123e09b9ab62d2b1fc694b0
Subodh-112 <subodhchauhan8880@gmail.com>
2023-03-28 23:43:13 +0530

    follow timer check

--- src/server.ts
+++ src/server.ts
@@ -48,7 +48,7 @@ let todayDate = new Date()
   .split(",")[0];
 
 //follow up Messages
-cron.schedule(" 40 23 * * *", () => {
+cron.schedule(" 55 23 * * *", () => {
   try {
     MongoService.collection("followUp")
       .find({})
