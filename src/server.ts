a2d0b85fafcbcfc96847cf478686697277c017b5
Subodh-112 <subodhchauhan8880@gmail.com>
2023-03-28 18:50:51 +0530

    new

--- src/server.ts
+++ src/server.ts
@@ -48,7 +48,7 @@ let todayDate = new Date()
   .split(",")[0];
 
 //follow up Messages
-cron.schedule(" 50 18 * * *", () => {
+cron.schedule(" 59 18 * * *", () => {
   try {
     MongoService.collection("followUp")
       .find({})
